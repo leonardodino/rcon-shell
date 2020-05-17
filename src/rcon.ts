31b6f31163b5ce7e2f9f98c8cacb5b33957357cb
leonardodino <leonardo@kunstdesign.com.br>
2020-05-17 20:51:43 +0100

    :zap: under 1k now

--- src/rcon.ts
+++ src/rcon.ts
@@ -16,25 +16,25 @@ type Events = {
 }
 
 export class Rcon extends EventEmitter<Events> {
-  #host: string
-  #port: number
-  #password: string
-  #token: null | string
-  #socket: null | Socket
+  private _host: string
+  private _port: number
+  private _password: string
+  private _token: null | string
+  private _socket: null | Socket
   constructor(settings: { host: string; port: number; password: string }) {
     super()
-    this.#host = settings.host
-    this.#port = settings.port
-    this.#password = settings.password
-    this.#token = null
-    this.#socket = null
+    this._host = settings.host
+    this._port = settings.port
+    this._password = settings.password
+    this._token = null
+    this._socket = null
   }
 
   connect() {
-    this.#socket = createSocket('udp4')
-    this.#socket
-      .on('listening', this.#handleSocketListening)
-      .on('message', this.#handleSocketMessage)
+    this._socket = createSocket('udp4')
+    this._socket
+      .on('listening', this._handleSocketListening)
+      .on('message', this._handleSocketMessage)
       .on('error', (error) => this.emit('error', error))
       .on('close', () => this.emit('close'))
       .bind(0)
@@ -43,26 +43,26 @@ export class Rcon extends EventEmitter<Events> {
   }
 
   send(data: string) {
-    if (!this.#token) return this.emit('error', NOT_AUTHENTICATED)
-    const parts = ['rcon', this.#token, this.#password, data.trim()]
-    this.#sendSocket(parts.filter(Boolean))
+    if (!this._token) return this.emit('error', NOT_AUTHENTICATED)
+    const parts = ['rcon', this._token, this._password, data.trim()]
+    this._sendSocket(parts.filter(Boolean))
   }
 
   disconnect() {
-    this.#socket?.close()
+    this._socket?.close()
     return this
   }
 
-  #sendSocket = (parts: string[]) => {
-    if (!this.#socket) return this.emit('error', NOT_CONNECTED)
+  private _sendSocket = (parts: string[]) => {
+    if (!this._socket) return this.emit('error', NOT_CONNECTED)
     const string = parts.join(' ') + '\n'
     const buffer = Buffer.alloc(offset + Buffer.byteLength(string))
     buffer.writeInt32LE(-1, 0)
     buffer.write(string, offset)
-    this.#socket.send(buffer, 0, buffer.length, this.#port, this.#host)
+    this._socket.send(buffer, 0, buffer.length, this._port, this._host)
   }
 
-  #handleSocketMessage = (data: Buffer) => {
+  private _handleSocketMessage = (data: Buffer) => {
     if (data.readUInt32LE(0) !== 0xffffffff) {
       return this.emit('error', new Error('Received malformed packet'))
     }
@@ -70,15 +70,15 @@ export class Rcon extends EventEmitter<Events> {
     const string = data.toString('utf-8', offset)
     const parts = string.split(' ')
     if (parts.length === 3 && parts[0] === 'challenge' && parts[1] === 'rcon') {
-      this.#token = parts[2].substr(0, parts[2].length - 1).trim()
+      this._token = parts[2].substr(0, parts[2].length - 1).trim()
       return this.emit('auth')
     }
 
     return this.emit('response', string.substr(1, string.length - 2))
   }
 
-  #handleSocketListening = () => {
+  private _handleSocketListening = () => {
     this.emit('connect')
-    this.#sendSocket(['challenge', 'rcon'])
+    this._sendSocket(['challenge', 'rcon'])
   }
 }
