33a7f740e441cd04a31794200895fc370d0aa135
leonardodino <leonardo@kunstdesign.com.br>
2020-05-25 06:24:30 +0100

    :recycle: minor cleanup in completions

--- src/completions.ts
+++ src/completions.ts
@@ -1,9 +1,10 @@
 import type { RconClient } from './rcon-client'
 
 const separatorRegExp = /^-+$/
+const cvarRegExp = /^([^_:\s][^:\s]+)\s*:.*, sv$/
 
 const parseList = (string: string, done = -1) => {
-  let output = new Set<string>()
+  const output = new Set<string>()
   const lines = string.replace(/[^\x00-\x7F]+/g, '').split('\n')
   for (const _line of lines) {
     const line = _line.trim()
@@ -16,22 +17,14 @@ const parseList = (string: string, done = -1) => {
 
 const parseCmdList = (string: string) => {
   const cmdlist = parseList(string)
-  cmdlist.delete('changelevel2')
-  return cmdlist
+  return cmdlist.delete('changelevel2'), cmdlist
 }
 
-const parseCvarList = (string: string, done = -1) => {
-  let output = new Set<string>()
-  const lines = string.replace(/[^\x00-\x7F]+/g, '').split('\n')
-  for (const _line of lines) {
-    const line = _line.trim()
-    if (separatorRegExp.test(line) && !++done) continue
-    if (done === 1) break
-    if (done === 0 && line.includes(':')) {
-      if (line.startsWith('_') || !line.endsWith(', sv')) continue
-      const cvar = line.split(':')[0]?.trim()
-      if (cvar) output.add(cvar)
-    }
+const parseCvarList = (string: string) => {
+  const output = new Set<string>()
+  for (const line of parseList(string)) {
+    const cvar = (line.match(cvarRegExp) || [])[1]
+    if (cvar) output.add(cvar)
   }
   return output
 }
@@ -40,14 +33,12 @@ export class Completions {
   private readonly _client: RconClient
   private _cmds: string[] = []
   private _maps: string[] = []
-  private _busy: boolean = false
 
   constructor(client: RconClient) {
     this._client = client.clone()
   }
 
   async load() {
-    if (this._busy) this._busy = true
     await this._client.connect()
 
     const cmdlist = parseCmdList(await this._client.send('cmdlist'))
@@ -64,7 +55,6 @@ export class Completions {
     }
 
     await this._client.disconnect()
-    this._busy = false
   }
 
   completer = (line: string): [string[], string] => {
